e05aed6712bbf48ed39a2d2bb8bdb09a84a704e9
Kavitha <Kavitha@DESKTOP-LC74QQG.attlocal.net>
2024-12-18 19:08:23 -0600

    First Commit

---
+++ GitDemo/GitDemoClass.java
@@ -0,0 +1,10 @@
+package GitDemo;
+
+public class GitDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub test package");
+	}
+
+}
